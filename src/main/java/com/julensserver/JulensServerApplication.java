bfd6167a6f574014757696930391da04a4ad3b92
JaeMin <jhprt0012@kyonggi.ac.kr>
2026-06-29 13:44:11 +0900

    chore: initialize JULens Spring Boot project

---
+++ src/main/java/com/julensserver/JulensServerApplication.java
@@ -0,0 +1,13 @@
+package com.julensserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulensServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulensServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julensserver/JulensServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julensserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulensServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
