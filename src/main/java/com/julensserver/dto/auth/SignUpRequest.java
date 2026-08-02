2e13abf8557608d2468a71b840e8c9eeba177081
JaeMin <jhprt0012@kyonggi.ac.kr>
2026-08-02 12:21:47 +0900

    feat: DTO Request 수정

--- src/main/java/com/julensserver/dto/auth/SignUpRequest.java
+++ src/main/java/com/julensserver/dto/auth/SignUpRequest.java
@@ -1,6 +1,7 @@
 package com.julensserver.dto.auth;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SignUpRequest {
+    @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "이메일 형식이어야 합니다.")
     @Size(max = 30, message = "이메일은 30자 미만이어야 합니다.")
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 10,  max = 20, message = "비밀번호는 10자 이상, 20자 미만이어야 합니다.")
     private String password;
 
