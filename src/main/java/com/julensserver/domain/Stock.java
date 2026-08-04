76cd437f9d9c5cee15abc90b37268f4dc59f37df
JaeMin <jhprt0012@kyonggi.ac.kr>
2026-08-04 17:00:16 +0900

    feat: 주식 분석기 도메인 추가 중

---
+++ src/main/java/com/julensserver/domain/Stock.java
@@ -0,0 +1,52 @@
+package com.julensserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stocks")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String ticker;
+
+    @Column(nullable = false, length = 200)
+    private String companyName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private Exchange exchange;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Currency currency;
+
+    @Column(length = 100)
+    private String sector;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    public Stock(
+            String ticker,
+            String companyName,
+            Exchange exchange,
+            Currency currency,
+            String sector
+    ){
+        this.ticker=ticker;
+        this.companyName=companyName;
+        this.exchange=exchange;
+        this.currency=currency;
+        this.sector=sector;
+    }
+}
